4c2930c207d43b580b41855dbe2c313b0525ba03
Adham Nasser <adhamxiii10@gmail.com>
2023-08-16 17:42:08 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Calculator 🧮
+
+This is a simple web-based calculator application built using HTML, CSS, and JavaScript. It allows users to perform basic arithmetic calculations and provides a user-friendly interface.
+
+## Table of Contents
+
+- [Features](#features)
+- [Usage](#usage)
+- [Live Demo](#live-demo)
+- [Technologies Used](#technologies-used)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- Addition, subtraction, multiplication, and division operations.
+- Ability to clear the current input, delete a single digit, and toggle the sign of the current number.
+- Percentage calculation functionality.
+- Dark mode for improved visibility in low-light environments.
+
+## Usage
+
+1. Launch the calculator by opening the `index.html` file in a web browser.
+2. Enter numbers and perform calculations using the provided buttons.
+3. The display at the top shows the current and previous operands, as well as the result of the calculation.
+4. Toggle dark mode by clicking the "Dark Mode" switch.
+
+## Live Demo
+
+Check out the live demo of the calculator: [Calculator Demo](https://your-username.github.io/calculator-demo)
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- [FontAwesome](https://fontawesome.com/) - Icons used in the UI
+- [Google Fonts](https://fonts.google.com/) - Used for the Inter font
+
+## Contributing
+
+Contributions to this calculator project are welcome! Here are a few ways you can contribute:
+
+- Report issues or suggest enhancements through [GitHub Issues](https://github.com/your-username/calculator/issues).
+- Fork the repository and create a pull request to suggest changes.
+
---
+++ script.js
@@ -0,0 +1,168 @@
+"use strict";
+
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+
+  plusMinus() {
+    this.currentOperand = -parseFloat(this.currentOperand);
+  }
+
+  percent() {
+    this.currentOperand = parseFloat(this.currentOperand) / 100;
+  }
+
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
+
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "/":
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
+  }
+
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(
+      this.currentOperand
+    );
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`;
+    }
+  }
+}
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const deleteButton = document.querySelector("[data-delete]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
+const plusMinusButton = document.querySelector("[data-plus-minus]");
+const percentButton = document.querySelector("[data-percent]");
+
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+equalsButton.addEventListener("click", () => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+allClearButton.addEventListener("click", () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+deleteButton.addEventListener("click", () => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
+
+plusMinusButton.addEventListener("click", () => {
+  calculator.plusMinus();
+  calculator.updateDisplay();
+});
+
+percentButton.addEventListener("click", () => {
+  calculator.percent();
+  calculator.updateDisplay();
+});
+
+const darkModeToggle = document.getElementById("mode");
+const body = document.getElementById("body");
+
+function toggleDarkMode() {
+  body.classList.toggle("dark-mode");
+  const isDarkMode = body.classList.contains("dark-mode");
+  localStorage.setItem("darkMode", isDarkMode);
+}
+
+if (localStorage.getItem("darkMode") === "true") {
+  body.classList.add("dark-mode");
+}
+
+darkModeToggle.addEventListener("change", toggleDarkMode);
